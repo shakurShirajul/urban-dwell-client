653869c336fa8b5d7f70ac57af8c64bfbe5aaad5
shakurShirajul <siirajulisllam@gmail.com>
2024-06-09 11:08:09 +0600

    React Map Added

--- src/Pages/Home/Map.jsx
+++ src/Pages/Home/Map.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const position = [51.505, -0.09]; // Default position for the map
-
+const position = [40.748817, -73.985428];
 const Map = () => {
     return (
         <div>
-            <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+            <MapContainer center={position} zoom={13} style={{ height: "50vh" }}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
                 <Marker position={position}>
                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
+                        Empire State Building<br /> 350 5th Avenue, New York, NY 10118.
                     </Popup>
                 </Marker>
             </MapContainer>

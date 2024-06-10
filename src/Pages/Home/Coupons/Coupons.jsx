15149abe3140054766d6fa61a3c751274734d950
shakurShirajul <siirajulisllam@gmail.com>
2024-06-10 18:59:23 +0600

    Coupons Added

--- src/Pages/Home/Coupons/Coupons.jsx
+++ src/Pages/Home/Coupons/Coupons.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import useAxiosPublic from '../../../hooks/useAxiosPublic';
 import { useQuery } from '@tanstack/react-query';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import { FreeMode, Pagination } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -18,17 +20,39 @@ const Coupons = () => {
         }
     })
 
-    console.log(coponsDisplay)
+    // console.log(coponsDisplay)
+    console.log(coponsDisplays);
 
     return (
         <div>
-            {
-                coponsDisplays.map((coponsDisplay)=>
-                    <SwiperSlide>
-
-                    </SwiperSlide>
-                )
-            }
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                freeMode={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[FreeMode, Pagination]}
+                className="mySwiper"
+            >
+                <div>
+                    {
+                        coponsDisplays.map(couponsDisplay =>
+                            <SwiperSlide>
+                                <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden my-10 border">
+                                    <div className="px-6 py-4">
+                                        <div className="font-bold text-xl mb-2 text-center text-gray-800">Get Coupon!</div>
+                                        <p className="text-gray-700 text-lg font-semibold text-center">
+                                            {couponsDisplay.coupon_Description}
+                                        </p>
+                                        <p className="text-3xl text-indigo-600 text-center mt-2 font-bold">{couponsDisplay.coupon_Code}</p>
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                        )
+                    }
+                </div>
+            </Swiper>
         </div>
     );
 };

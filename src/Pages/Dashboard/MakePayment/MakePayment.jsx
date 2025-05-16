8dc633a00402e5a769194b7831228cc680f305bd
shakurShirajul <siirajulisllam@gmail.com>
2025-05-16 16:30:20 +0600

    Payment History Page Updated

--- src/Pages/Dashboard/MakePayment/MakePayment.jsx
+++ src/Pages/Dashboard/MakePayment/MakePayment.jsx
@@ -1,126 +1,161 @@
-import { useQuery } from '@tanstack/react-query';
-import useAxiosSecure from '../../../hooks/userAxiosSecure';
-import { useContext } from 'react';
-import { AuthContext } from '../../../Providers/AuthProviders';
-import { useNavigate } from 'react-router-dom';
-
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/userAxiosSecure";
+import { useContext } from "react";
+import { AuthContext } from "../../../Providers/AuthProviders";
+import { useNavigate } from "react-router-dom";
 
 const MakePayment = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const axiosSecure = useAxiosSecure();
-    const { user } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const {
+    data: userPaymentData = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["userPaymentData"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/specific/${user.email}`, {
+        withCredentials: true,
+      });
+      return res.data;
+    },
+  });
 
-    const { data: userPaymentData = [], refetch } = useQuery({
-        queryKey: ['userPaymentData'],
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/users/specific/${user.email}`, { withCredentials: true })
-            return res.data;
-        }
-    })
+  const { user_name, user_email, block_name, floor_no, apartment_no, rent } =
+    userPaymentData;
 
-    const { user_name, user_email, block_name, floor_no, apartment_no, rent } = userPaymentData;
+  const handleMakePaymentForm = (event) => {
+    event.preventDefault();
+    const paymentMonth = event.target.calender.value;
 
-    const handleMakePaymentForm = (event) => {
-        event.preventDefault();
-        const paymentMonth = event.target.calender.value;
+    const paymentData = {
+      month: paymentMonth,
+      amount: rent,
+    };
 
-        const paymentData = {
-            month: paymentMonth,
-            amount: rent,
-        }
-
-        navigate(`/dashboard/paymentpage`, { state: paymentData });
-    }
-    return (
+    navigate(`/dashboard/paymentpage`, { state: paymentData });
+  };
+  return (
+    <div>
+      {isLoading ? (
+        <div className="flex h-screen items-center justify-center space-x-2">
+          <div className="w-4 h-4 rounded-full animate-pulse bg-primary"></div>
+          <div className="w-4 h-4 rounded-full animate-pulse bg-primary"></div>
+          <div className="w-4 h-4 rounded-full animate-pulse bg-primary"></div>
+        </div>
+      ) : (
         <div>
-            <section className="p-6">
-                <form onSubmit={handleMakePaymentForm} className="container flex flex-col mx-auto space-y-12">
-                    <fieldset className="p-6 rounded-xl border border-gray-600 shadow-sm dark:bg-gray-50">
-                        <h1 className='text-4xl font-extrabold text-gray-800 mb-5'>Make Payment:</h1>
-                        <div className='space-y-5 font-roboto'>
-                            <div className="flex flex-col lg:flex-row gap-5">
-                                <div className='flex-1 space-y-2'>
-                                    <label className="text-lg font-medium">Name: </label>
-                                    <input
-                                        type="text"
-                                        disabled
-                                        value={user_name}
-                                        className="w-full text-black rounded-lg py-2.5 pl-2 border" />
-                                </div>
-                                <div className='flex-1 space-y-2'>
-                                    <label className="text-lg font-medium">Email: </label>
-                                    <input
-                                        type="email"
-                                        disabled
-                                        value={user_email}
-                                        className="w-full text-black rounded-lg py-2.5 pl-2 border" />
-                                </div>
-                            </div>
-                            <div className='flex flex-col lg:flex-row gap-5'>
-                                <div className='flex-1 space-y-2'>
-                                    <p className="text-lg font-medium">Block Name: </p>
-                                    <input
-                                        type="text"
-                                        disabled
-                                        value={block_name}
-                                        className="w-full text-black rounded-lg py-2.5 pl-2 border" />
-                                </div>
-                                <div className='flex-1 space-y-2'>
-                                    <p className="text-lg font-medium">Floor No:</p>
-                                    <input
-                                        disabled
-                                        value={floor_no}
-                                        type="text"
-                                        className="w-full text-black rounded-lg py-2.5 pl-2 border" />
-                                </div>
-                                <div className='flex-1 space-y-2'>
-                                    <p className="text-lg font-medium">Room No:</p>
-                                    <input
-                                        type="text"
-                                        disabled
-                                        value={apartment_no}
-                                        className="w-full text-black rounded-lg py-2.5 pl-2 border" />
-                                </div>
-                                <div className='flex-1 space-y-2'>
-                                    <label className="text-lg font-medium">Rent:</label>
-                                    <input
-                                        type="text"
-                                        disabled
-                                        value={`$${rent}`}
-                                        className="w-full text-black rounded-lg py-2.5 pl-2 border" />
-                                </div>
-                                <div className='flex-1 space-y-2'>
-                                    <label className="text-lg font-medium">Payment Month:</label>
-                                    <select
-                                        name='calender'
-                                        required
-                                        className="select select-bordered w-full max-w-xs focus:outline-none">
-                                        <option disabled selected value="">Select A Month</option>
-                                        <option value="january">January</option>
-                                        <option value="february">February</option>
-                                        <option value="march">March</option>
-                                        <option value="april">April</option>
-                                        <option value="may">May</option>
-                                        <option value="june">June</option>
-                                        <option value="july">July</option>
-                                        <option value="august">August</option>
-                                        <option value="september">September</option>
-                                        <option value="october">October</option>
-                                        <option value="november">November</option>
-                                        <option value="december">December</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className='flex justify-end'>
-                                <input type="submit" value="Submit" className="btn btn-success text-white text-base" />
-                            </div>
-                        </div>
-                    </fieldset>
-                </form>
-            </section>
-        </div >
-    );
+          <section className="font-mulish">
+            <h1 className="text-4xl font-extrabold text-gray-800 mb-5">
+              Make Payment:
+            </h1>
+            <form
+              onSubmit={handleMakePaymentForm}
+              className="container flex flex-col mx-auto space-y-12"
+            >
+              <fieldset className="p-6 rounded-xl border border-gray-50 shadow-md bg-gray-100">
+                <div className="space-y-5 font-roboto">
+                  <div className="flex flex-col lg:flex-row gap-5">
+                    <div className="flex-1 space-y-2">
+                      <label className="text-lg font-medium">Name: </label>
+                      <input
+                        type="text"
+                        disabled
+                        value={user_name}
+                        className="w-full text-black rounded-lg py-2.5 pl-2 border"
+                      />
+                    </div>
+                    <div className="flex-1 space-y-2">
+                      <label className="text-lg font-medium">Email: </label>
+                      <input
+                        type="email"
+                        disabled
+                        value={user_email}
+                        className="w-full text-black rounded-lg py-2.5 pl-2 border"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col lg:flex-row gap-5">
+                    <div className="flex-1 space-y-2">
+                      <p className="text-lg font-medium">Block Name: </p>
+                      <input
+                        type="text"
+                        disabled
+                        value={block_name}
+                        className="w-full text-black rounded-lg py-2.5 pl-2 border"
+                      />
+                    </div>
+                    <div className="flex-1 space-y-2">
+                      <p className="text-lg font-medium">Floor No:</p>
+                      <input
+                        disabled
+                        value={floor_no}
+                        type="text"
+                        className="w-full text-black rounded-lg py-2.5 pl-2 border"
+                      />
+                    </div>
+                    <div className="flex-1 space-y-2">
+                      <p className="text-lg font-medium">Room No:</p>
+                      <input
+                        type="text"
+                        disabled
+                        value={apartment_no}
+                        className="w-full text-black rounded-lg py-2.5 pl-2 border"
+                      />
+                    </div>
+                    <div className="flex-1 space-y-2">
+                      <label className="text-lg font-medium">Rent:</label>
+                      <input
+                        type="text"
+                        disabled
+                        value={`$${rent}`}
+                        className="w-full text-black rounded-lg py-2.5 pl-2 border"
+                      />
+                    </div>
+                    <div className="flex-1 space-y-2">
+                      <label className="text-lg font-medium">
+                        Payment Month:
+                      </label>
+                      <select
+                        name="calender"
+                        required
+                        className="select select-bordered w-full max-w-xs focus:outline-none"
+                      >
+                        <option disabled selected value="">
+                          Select A Month
+                        </option>
+                        <option value="january">January</option>
+                        <option value="february">February</option>
+                        <option value="march">March</option>
+                        <option value="april">April</option>
+                        <option value="may">May</option>
+                        <option value="june">June</option>
+                        <option value="july">July</option>
+                        <option value="august">August</option>
+                        <option value="september">September</option>
+                        <option value="october">October</option>
+                        <option value="november">November</option>
+                        <option value="december">December</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="flex justify-end">
+                    <input
+                      type="submit"
+                      value="Submit"
+                      className="btn btn-success text-white text-base"
+                    />
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          </section>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MakePayment;
--- src/Pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -1,110 +1,123 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import useAxiosSecure from '../../../hooks/userAxiosSecure';
-import { useContext } from 'react';
-import { AuthContext } from '../../../Providers/AuthProviders';
-import PaymentHistoryTable from './PaymentHistoryTable';
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/userAxiosSecure";
+import { useContext } from "react";
+import { AuthContext } from "../../../Providers/AuthProviders";
+import PaymentHistoryTable from "./PaymentHistoryTable";
 
 const PaymentHistory = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user } = useContext(AuthContext);
+  const queryClient = useQueryClient();
 
-    const axiosSecure = useAxiosSecure();
-    const { user } = useContext(AuthContext);
-    const queryClient = useQueryClient();
+  const months = [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+  ];
 
-    const months = [
-        "january",
-        "february",
-        "march",
-        "april",
-        "may",
-        "june",
-        "july",
-        "august",
-        "september",
-        "october",
-        "november",
-        "december"
-    ];
+  let url = `/apartment-rent-info?email=${user.email}`;
 
-    let url = `/apartment-rent-info?email=${user.email}`;
-    
-    const { data: paymentHistorys = [], refetch } = useQuery({
-        queryKey: ['paymentHistorys'],
-        queryFn: async () => {
-            const response = await axiosSecure.get(url, { withCredentials: true });
-            return response.data;
-        }
-    })
+  const {
+    data: paymentHistorys = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["paymentHistorys"],
+    queryFn: async () => {
+      const response = await axiosSecure.get(url, { withCredentials: true });
+      return response.data;
+    },
+  });
 
-    const handleSearchButton = (event) => {
-        event.preventDefault();
+  const handleSearchButton = (event) => {
+    event.preventDefault();
 
-        const searchInput = event.target.searchInput.value;
+    const searchInput = event.target.searchInput.value;
 
-        console.log(searchInput);
+    console.log(searchInput);
 
-        if (searchInput > 0 && searchInput < 13) {
-            const month = months[searchInput - 1];
-            url = `/apartment-rent-info/search?email=${user.email}&month=${month}`
-        }
-        else {
-            const lowerCaseSearchInput = searchInput.toLowerCase()
-            url = `/apartment-rent-info/search?email=${user.email}&month=${lowerCaseSearchInput}`
-        }
-
-        const res = axiosSecure.get(url);
-
-        queryClient.setQueryData(['paymentHistorys'], res.data);
-        refetch();
+    if (searchInput > 0 && searchInput < 13) {
+      const month = months[searchInput - 1];
+      url = `/apartment-rent-info/search?email=${user.email}&month=${month}`;
+    } else {
+      const lowerCaseSearchInput = searchInput.toLowerCase();
+      url = `/apartment-rent-info/search?email=${user.email}&month=${lowerCaseSearchInput}`;
     }
 
-    return (
-        <div className='font-roboto'>
+    const res = axiosSecure.get(url);
 
-            <div className='space-y-5 mb-10'>
-                <div>
-                    <h1 className='text-3xl font-semibold'>
-                        {user.displayName}'s <br /> <span className='text-[#274c07]'>Payment History</span>
-                    </h1>
-                </div>
-                <form onSubmit={handleSearchButton} className="flex items-center max-w-xl mx-auto ">
-                    <input
-                        type="text"
-                        name="searchInput"
-                        placeholder="Search By month Name or Month Number"
-                        className="flex-1 p-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
-                    <input type='submit' className="p-2 bg-blue-500 text-white border border-blue-500 rounded-r-md hover:bg-blue-600"
-                        value="Search" />
-                </form>
-            </div>
-            <div className="overflow-x-auto border rounded-xl p-5">
-                <table className="table table-zebra">
+    queryClient.setQueryData(["paymentHistorys"], res.data);
+    refetch();
+  };
 
-                    <thead>
-                        <tr className='md:text-base lg:text-lg'>
-                            <th></th>
-                            <th>Date (DD-MM-YY)</th>
-                            <th>Transaction ID</th>
-                            <th>Month</th>
-                            <th>Coupon</th>
-                            <th>Discount</th>
-                            <th>Amount Pay</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            paymentHistorys.map((paymentHistory, index) =>
-                                <PaymentHistoryTable
-                                    index={index}
-                                    key={paymentHistory._id}
-                                    paymentHistory={paymentHistory} />
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
+  return (
+    <div>
+      {isLoading ? (
+        <div className="flex h-screen items-center justify-center space-x-2">
+          <div className="w-4 h-4 rounded-full animate-pulse bg-primary"></div>
+          <div className="w-4 h-4 rounded-full animate-pulse bg-primary"></div>
+          <div className="w-4 h-4 rounded-full animate-pulse bg-primary"></div>
         </div>
-    );
+      ) : (
+        <div className="font-mulish">
+          <div className="space-y-5 mb-10">
+            <div>
+              <h1 className="text-4xl font-extrabold">Payment History</h1>
+            </div>
+            <form
+              onSubmit={handleSearchButton}
+              className="flex items-center max-w-xl mx-auto "
+            >
+              <input
+                type="text"
+                name="searchInput"
+                placeholder="Search By Month Name or Month Number"
+                className="flex-1 p-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <input
+                type="submit"
+                className="p-2 bg-primary text-white border border-blue-500 rounded-r-md hover:bg-blue-900"
+                value="Search"
+              />
+            </form>
+          </div>
+          <div className="overflow-x-auto border rounded-xl p-5">
+            <table className="table table-zebra">
+              <thead>
+                <tr className="md:text-base lg:text-lg">
+                  <th></th>
+                  <th>Date (DD-MM-YY)</th>
+                  <th>Transaction ID</th>
+                  <th>Month</th>
+                  <th>Coupon</th>
+                  <th>Discount</th>
+                  <th>Amount Pay</th>
+                </tr>
+              </thead>
+              <tbody>
+                {paymentHistorys.map((paymentHistory, index) => (
+                  <PaymentHistoryTable
+                    index={index}
+                    key={paymentHistory._id}
+                    paymentHistory={paymentHistory}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default PaymentHistory;

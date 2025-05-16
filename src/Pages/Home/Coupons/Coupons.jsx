3136b23850bcf283422b7d7dd2c8639d81b4bf72
shakurShirajul <siirajulisllam@gmail.com>
2025-05-16 12:00:27 +0600

    Map & Footer Updated

--- src/Pages/Home/Coupons/Coupons.jsx
+++ src/Pages/Home/Coupons/Coupons.jsx
@@ -1,57 +1,63 @@
-import React from 'react';
-import useAxiosPublic from '../../../hooks/useAxiosPublic';
-import { useQuery } from '@tanstack/react-query';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
-import { FreeMode, Pagination } from 'swiper/modules';
+import React from "react";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import { FreeMode, Pagination } from "swiper/modules";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/navigation";
 
 const Coupons = () => {
+  const axiosPublic = useAxiosPublic();
 
-    const axiosPublic = useAxiosPublic();
+  const { data: coponsDisplays = [], refetch } = useQuery({
+    queryKey: ["coponsDisplays"],
+    queryFn: async () => {
+      const response = await axiosPublic.get(`/coupon-code`, {
+        withCredentials: true,
+      });
+      return response.data;
+    },
+  });
 
-    const { data: coponsDisplays = [], refetch } = useQuery({
-        queryKey: ['coponsDisplays'],
-        queryFn: async () => {
-            const response = await axiosPublic.get(`/coupon-code`, { withCredentials: true });
-            return response.data;
-        }
-    })
-
-    return (
-        <div className='container mx-auto'>
-            <Swiper
-                slidesPerView={1}
-                spaceBetween={30}
-                freeMode={true}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[FreeMode, Pagination]}
-                className="mySwiper"
-            >
-                <div className='grid'>
-                    {
-                        coponsDisplays.map(couponsDisplay =>
-                            <SwiperSlide>
-                                <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden my-10 border">
-                                    <div className="px-6 py-4">
-                                        <div className="font-bold text-xl mb-2 text-center text-gray-800">Get Coupon!</div>
-                                        <p className="text-gray-700 text-lg font-semibold text-center">
-                                            {couponsDisplay.coupon_Description}
-                                        </p>
-                                        <p className="text-3xl text-indigo-600 text-center mt-2 font-bold">{couponsDisplay.coupon_Code}</p>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                        )
-                    }
+  return (
+    <div className="max-w-7xl mx-auto">
+      <h2 className="text-4xl text-center font-extrabold text-gray-800">
+        Get Exciting Coupon!
+      </h2>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        <div className="grid">
+          {coponsDisplays.map((couponsDisplay) => (
+            <SwiperSlide>
+              <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden my-10 border">
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2 text-center text-gray-800">
+                    Get Coupon!
+                  </div>
+                  <p className="text-gray-700 text-lg font-semibold text-center">
+                    {couponsDisplay.coupon_Description}
+                  </p>
+                  <p className="text-3xl text-indigo-600 text-center mt-2 font-bold">
+                    {couponsDisplay.coupon_Code}
+                  </p>
                 </div>
-            </Swiper>
+              </div>
+            </SwiperSlide>
+          ))}
         </div>
-    );
+      </Swiper>
+    </div>
+  );
 };
 
 export default Coupons;
--- src/Pages/Home/Location.jsx
+++ src/Pages/Home/Location.jsx
@@ -1,36 +1,41 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import Map from './Map';
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import Map from "./Map";
 
-const position = [51.505, -0.09]; 
+const position = [51.505, -0.09];
 
 const Location = () => {
-    return (
-        <div className='font-roboto'>
-            <div className='text-4xl font-extrabold text-gray-800 text-center'>Apartment Location</div>
-            <div className="p-5 mx-auto sm:p-10 md:p-16">
-                <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
-                    <div className='z-[0]'>
-                        <Map />
-                    </div>
-                    <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md border bg-gray-50 z-[10]">
-                        <div className="space-y-2">
-                            <p className="inline-block text-2xl font-semibold sm:text-3xl">Location:</p>
-                            <div className="dark:text-gray-800">
-                                <p>Empire State Building
-                                    <br />
-                                    350 5th Avenue
-                                    <br />
-                                    New York, NY 10118
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="font-roboto">
+      <div className="text-4xl font-extrabold text-gray-800 text-center">
+        Apartment Location
+      </div>
+      <div className="p-5 mx-auto sm:p-10 md:p-16">
+        <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
+          <div className="z-[0]">
+            <Map />
+          </div>
+          <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-xl shadow-lg bg-gray-50 z-[10]">
+            <div className="space-y-2">
+              <p className="inline-block text-2xl font-semibold sm:text-3xl">
+                Location:
+              </p>
+              <div className="dark:text-gray-800">
+                <p>
+                  Empire State Building
+                  <br />
+                  350 5th Avenue
+                  <br />
+                  New York, NY 10118
+                </p>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Location;

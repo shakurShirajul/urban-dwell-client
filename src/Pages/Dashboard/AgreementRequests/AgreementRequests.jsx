fd3410aed8b05db5d6500ec19f26a27a0bde6af2
shakurShirajul <siirajulisllam@gmail.com>
2025-05-16 15:04:58 +0600

    Agreement Request Page Updated

--- src/Pages/Dashboard/AgreementRequests/AgreementRequests.jsx
+++ src/Pages/Dashboard/AgreementRequests/AgreementRequests.jsx
@@ -1,70 +1,82 @@
-import AgreementRequestsTable from './AgreementRequestsTable';
-import { useQuery } from '@tanstack/react-query';
-import useAxiosSecure from '../../../hooks/userAxiosSecure';
-import { useContext } from 'react';
-import { AuthContext } from '../../../Providers/AuthProviders';
-import { ToastContainer } from 'react-toastify';
+import AgreementRequestsTable from "./AgreementRequestsTable";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/userAxiosSecure";
+import { useContext } from "react";
+import { AuthContext } from "../../../Providers/AuthProviders";
+import { ToastContainer } from "react-toastify";
 
 const AgreementRequests = () => {
+  const { user, successToast, updateToast } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
 
-    const { user, successToast, updateToast } = useContext(AuthContext);
-    const axiosSecure = useAxiosSecure();
+  const { data: agereementsRequests = [], refetch } = useQuery({
+    queryKey: ["agereementRequest"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/agreements/requests?email=${user.email}`,
+        { withCredentials: true }
+      );
+      return res.data;
+    },
+  });
 
-    const { data: agereementsRequests = [], refetch } = useQuery({
-        queryKey: ['agereementRequest'],
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/agreements/requests?email=${user.email}`, { withCredentials: true })
-            return res.data;
-        }
-    })
+  const handleAcceptButton = (id) => {
+    axiosSecure
+      .patch(
+        `/agreements/requests/updates?status=accepted&id=${id}&email=${user.email}`,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        refetch();
+        successToast("Agreement Accepted");
+      });
+  };
 
-    const handleAcceptButton = (id) => {
+  const handleRejectButton = (id) => {
+    axiosSecure
+      .patch(
+        `/agreements/requests/updates?status=rejected&id=${id}&email=${user.email}`,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        refetch();
+        updateToast("Agreement Rejected");
+      });
+  };
 
-        axiosSecure.patch(`/agreements/requests/updates?status=accepted&id=${id}&email=${user.email}`, { withCredentials: true })
-            .then(response => {
-                refetch();
-                successToast("Agreement Accepted");
-            })
-    }
+  return (
+    <div className="font-mulish">
+      <p className="text-4xl font-extrabold mb-5">Agreement Request</p>
 
-    const handleRejectButton = (id) => {
-        axiosSecure.patch(`/agreements/requests/updates?status=rejected&id=${id}&email=${user.email}`, { withCredentials: true })
-            .then(response => {
-                refetch();
-                updateToast("Agreement Rejected");
-            })
-    }
-
-    return (
-        <div className='font-mulish'>
-            <p className='text-4xl font-semibold mb-5'>AgreementRequest</p>
-
-            <div className="overflow-x-auto">
-                <table className="table">
-                    <thead className='text-lg text-black font-bold'>
-                        <tr>
-                            <th>User Name</th>
-                            <th>User Email</th>
-                            <th>Floor No</th>
-                            <th>Block Name</th>
-                            <th>Room No</th>
-                            <th>Rent</th>
-                            <th>Agreement Request Date</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            agereementsRequests.map(agereementRequest =>
-                                <AgreementRequestsTable key={agereementRequest._id} agereementRequest={agereementRequest} handleAcceptButton={handleAcceptButton} handleRejectButton={handleRejectButton} />
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
-            <ToastContainer/>
-        </div>
-    );
+      <div className="overflow-x-auto bg-gray-100 rounded-md shadow-md border border-gray-100">
+        <table className="table">
+          <thead className="text-lg text-black font-bold">
+            <tr>
+              <th>User Name</th>
+              <th>User Email</th>
+              <th>Floor No</th>
+              <th>Block Name</th>
+              <th>Room No</th>
+              <th>Rent</th>
+              <th>Agreement Request Date</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {agereementsRequests.map((agereementRequest) => (
+              <AgreementRequestsTable
+                key={agereementRequest._id}
+                agereementRequest={agereementRequest}
+                handleAcceptButton={handleAcceptButton}
+                handleRejectButton={handleRejectButton}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default AgreementRequests;
--- src/Pages/Dashboard/AgreementRequests/AgreementRequestsTable.jsx
+++ src/Pages/Dashboard/AgreementRequests/AgreementRequestsTable.jsx
@@ -1,74 +1,100 @@
-import CloseIcon from '@mui/icons-material/Close';
-import CheckIcon from '@mui/icons-material/Check';
+import CancelIcon from "@mui/icons-material/Cancel";
+import CheckIcon from "@mui/icons-material/Check";
 
-const AgreementRequestsTable = ({ agereementRequest, handleAcceptButton, handleRejectButton }) => {
-    const { _id, user_name, user_email, floor_no, block_name, apartment_no, rent, status, agreement_request_date } = agereementRequest;
+const AgreementRequestsTable = ({
+  agereementRequest,
+  handleAcceptButton,
+  handleRejectButton,
+}) => {
+  const {
+    _id,
+    user_name,
+    user_email,
+    floor_no,
+    block_name,
+    apartment_no,
+    rent,
+    status,
+    agreement_request_date,
+  } = agereementRequest;
 
-    const monthNames = [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
-    ];
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-    const currentDate = new Date(agreement_request_date);
-    const date = currentDate.getDate();
-    const month = currentDate.getMonth();
-    const year = currentDate.getFullYear();
+  const currentDate = new Date(agreement_request_date);
+  const date = currentDate.getDate();
+  const month = currentDate.getMonth();
+  const year = currentDate.getFullYear();
 
-    return (
-        <tr className='text-base font-medium'>
-            <td>
-                <div>
-                    <p>{user_name}</p>
-                </div>
-            </td>
-            <td>
-                <div>
-                    <p>{user_email}</p>
-                </div>
-            </td>
-            <td>
-                <div>
-                    <p>{floor_no}</p>
-                </div>
-            </td>
-            <td>
-                <div>
-                    <p>{block_name}</p>
-                </div>
-            </td>
-            <td>
-                <div>
-                    <p>{apartment_no}</p>
-                </div>
-            </td>
-            <td>
-                <div>
-                    <p>${rent}</p>
-                </div>
-            </td>
-            <td>
-                <div>
-                    <p>{`${date}th ${monthNames[month]} ${year}`}</p>
-                </div>
-            </td>
-            <td>
-                <div>
-                    <div className='flex gap-2'>
-                        <button
-                            onClick={() => handleRejectButton(_id)}
-                            className="btn btn-error">
-                            <CloseIcon className='text-white' />
-                        </button>
-                        <button
-                            onClick={() => handleAcceptButton(_id)}
-                            className="btn btn-success">
-                            <CheckIcon className='text-white' />
-                        </button>
-                    </div>
-                </div>
-            </td>
-        </tr>
-    );
+  return (
+    <tr className="text-base font-medium">
+      <td>
+        <div>
+          <p>{user_name}</p>
+        </div>
+      </td>
+      <td>
+        <div>
+          <p>{user_email}</p>
+        </div>
+      </td>
+      <td>
+        <div>
+          <p>{floor_no}</p>
+        </div>
+      </td>
+      <td>
+        <div>
+          <p>{block_name}</p>
+        </div>
+      </td>
+      <td>
+        <div>
+          <p>{apartment_no}</p>
+        </div>
+      </td>
+      <td>
+        <div>
+          <p>${rent}</p>
+        </div>
+      </td>
+      <td>
+        <div>
+          <p>{`${date}th ${monthNames[month]} ${year}`}</p>
+        </div>
+      </td>
+      <td>
+        <div>
+          <div className="flex gap-2">
+            <button
+              onClick={() => handleRejectButton(_id)}
+              className="btn bg-red-600 border-none hover:bg-red-800"
+            >
+              <CancelIcon className="text-white" />
+            </button>
+            <button
+              onClick={() => handleAcceptButton(_id)}
+              className="btn bg-green-500 border-none hover:bg-green-700"
+            >
+              <CheckIcon className="text-white" />
+            </button>
+          </div>
+        </div>
+      </td>
+    </tr>
+  );
 };
 
 export default AgreementRequestsTable;

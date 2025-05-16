d8d370c261ac4f3133a6a6a54ab8a1a2af88f550
shakurShirajul <siirajulisllam@gmail.com>
2025-05-16 14:53:16 +0600

    Anounce Page Updated

--- src/Pages/Dashboard/MakeAnnouncement/MakeAnnouncement.jsx
+++ src/Pages/Dashboard/MakeAnnouncement/MakeAnnouncement.jsx
@@ -1,41 +1,43 @@
-import React, { useContext } from 'react';
-import MakeAnnouncementForm from './MakeAnnouncementForm';
-import { AuthContext } from '../../../Providers/AuthProviders';
-import { ToastContainer } from 'react-toastify';
-import useAxiosSecure from '../../../hooks/userAxiosSecure';
+import React, { useContext } from "react";
+import MakeAnnouncementForm from "./MakeAnnouncementForm";
+import { AuthContext } from "../../../Providers/AuthProviders";
+import { ToastContainer } from "react-toastify";
+import useAxiosSecure from "../../../hooks/userAxiosSecure";
 
 const MakeAnnouncement = () => {
-    const { user,successToast } = useContext(AuthContext);
+  const { user, successToast } = useContext(AuthContext);
 
-    const axiosSecure = useAxiosSecure();
+  const axiosSecure = useAxiosSecure();
 
-    const handleAnnounce = (event) => {
-        event.preventDefault();
+  const handleAnnounce = (event) => {
+    event.preventDefault();
 
-        const form = event.target;
+    const form = event.target;
 
-        const announcementData = {
-            announce_title: form.title.value,
-            announce_description: form.description.value,
-            announce_author_email: user.email,
-        }
+    const announcementData = {
+      announce_title: form.title.value,
+      announce_description: form.description.value,
+      announce_author_email: user.email,
+    };
 
-        axiosSecure.post(`/announcement?email=${user.email}`, announcementData, { withCredentials: true })
-        .then(res=>{
-            successToast("Announcement Created");
-            form.reset();
-        })
-
-    }
-    return (
-        <div>
-            <div className='space-y-5'>
-                <p className='text-4xl font-semibold'>Make An Announcement</p>
-                <MakeAnnouncementForm handleAnnounce={handleAnnounce}/>
-            </div>
-            <ToastContainer/>
-        </div>
-    );
+    axiosSecure
+      .post(`/announcement?email=${user.email}`, announcementData, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        successToast("Announcement Created");
+        form.reset();
+      });
+  };
+  return (
+    <div>
+      <div className="font-mulish space-y-5 ">
+        <p className="text-4xl font-extrabold">Make An Announcement</p>
+        <MakeAnnouncementForm handleAnnounce={handleAnnounce} />
+      </div>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default MakeAnnouncement;
--- src/Pages/Dashboard/MakeAnnouncement/MakeAnnouncementForm.jsx
+++ src/Pages/Dashboard/MakeAnnouncement/MakeAnnouncementForm.jsx
@@ -1,27 +1,46 @@
-import React from 'react';
+import React from "react";
+import CampaignIcon from "@mui/icons-material/Campaign";
 
-const MakeAnnouncementForm = ({handleAnnounce}) => {
-    return (
-        <div className='font-poppins border rounded-xl'>
-            <form onSubmit={handleAnnounce} className="container flex flex-col mx-auto space-y-12 rounded-xl">
-                <fieldset className="p-6 rounded-xl shadow-sm dark:bg-gray-50 space-y-5" >
-                    <div className="">
-                        <label className="text-lg">Title:</label>
-                        <div>
-                            <input type="text" required name='title' placeholder="Announcement Title" className="input input-bordered w-full" />
-                        </div>
-                    </div>
-                    <div className="">
-                        <label className="text-lg">Description:</label>
-                        <div>
-                            <textarea rows='10' required name='description' className="textarea textarea-bordered w-full" placeholder="Announcement Description"></textarea>
-                        </div>
-                    </div>
-                    <input type="submit" className="btn btn-success text-white" value="Announce" />
-                </fieldset>
-            </form>
-        </div>
-    );
+const MakeAnnouncementForm = ({ handleAnnounce }) => {
+  return (
+    <div className="border rounded-xl shadow-md text-gray-800">
+      <form
+        onSubmit={handleAnnounce}
+        className="container flex flex-col mx-auto space-y-12 rounded-xl"
+      >
+        <fieldset className="p-6 rounded-xl shadow-sm dark:bg-gray-100 space-y-5">
+          <div className="">
+            <label className="text-lg font-semibold">Title:</label>
+            <div>
+              <input
+                type="text"
+                required
+                name="title"
+                placeholder="Announcement Title"
+                className="input input-bordered w-full"
+              />
+            </div>
+          </div>
+          <div className="">
+            <label className="text-lg font-semibold">Description:</label>
+            <div>
+              <textarea
+                rows="10"
+                required
+                name="description"
+                className="textarea textarea-bordered w-full"
+                placeholder="Announcement Description"
+              ></textarea>
+            </div>
+          </div>
+          <button className="flex items-center  justify-center btn bg-green-500 hover:bg-green-600 border-none text-white">
+            <CampaignIcon />
+            <span>Announce</span>
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default MakeAnnouncementForm;

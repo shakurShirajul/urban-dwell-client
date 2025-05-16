5082a48d78b249cef40fde1972f30a7ad2e1a6df
shakurShirajul <siirajulisllam@gmail.com>
2025-05-16 10:31:21 +0600

    Banner Sized Fixwd

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
-import { Helmet } from 'react-helmet-async';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import { Helmet } from "react-helmet-async";
 
 const Root = () => {
-    return (
-        <div>
-            <Helmet>
-                <title>Home | Urban Dwell</title>
-            </Helmet>
-            <Nav></Nav>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Home | Urban Dwell</title>
+      </Helmet>
+      <Nav></Nav>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,22 +1,22 @@
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
-
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Banner = () => {
-    return (
-        <Carousel autoPlay interval={2000} infiniteLoop>
-            <div>
-                <img src="https://i.ibb.co/bX1FfYS/8802751-ai.png" />
-            </div>
-            <div>
-                <img src="https://i.ibb.co/b6tz3YN/9226083-ai.png" />
-            </div>
-            <div>
-                <img src="https://i.ibb.co/vzRn4PJ/7340259-ai.png" />
-            </div>
-        </Carousel>
-    );    
+  return (
+    <div className="pt-20">
+      <Carousel autoPlay interval={2000} infiniteLoop>
+        <div>
+          <img src="https://i.ibb.co/bX1FfYS/8802751-ai.png" />
+        </div>
+        <div>
+          <img src="https://i.ibb.co/b6tz3YN/9226083-ai.png" />
+        </div>
+        <div>
+          <img src="https://i.ibb.co/vzRn4PJ/7340259-ai.png" />
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
-
